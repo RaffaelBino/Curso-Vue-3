ad3827f407dd7811ff30509cca1deafdcf692149
Raffael Bino <raffael_bino18@yahoo.com.br>
2021-05-20 11:44:22 -0300

    Adicionando arquivos do curso de Vue 3

---
+++ data-binding/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration}">Hello {{ name }}!</h1>
+    <input type="text" v-model="name">
+    <br>
+
+    <a :href="link">Link para o perfil</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Raffael',
+    link: 'https://raffaelbino.github.io',
+    decoration: 'underline'
+  }),
+};
+</script>
+
+<style></style>
---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>LifeCycle Hooks</h1>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+
+export default {
+ setup() {
+   onMounted(() => {
+     console.log('Foi montado com Composition API');
+   })
+ }
+}
+// export default {
+//  mounted() {
+//    console.log('Foi montado o Options API');
+//  }
+// }
+</script>
+
+<style>
+
+</style>
